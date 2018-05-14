7ab5f6dd4ed830cf55b95a5b27600d0774fb631d
Is-Yang <1255786936@qq.com>
2018-05-14 11:55:59 +0800

    print lesson

--- public_dev/js/taughted_record.js
+++ public_dev/js/taughted_record.js
@@ -166,19 +166,28 @@ $(function(){
     /* Sort End */
     // 发送 email
     $('#sendEmail').on('click', function() {
-        var email=prompt("Please enter your email address:");  
+        $("#emailInput").val("");
+        $(".el-message-box__wrapper , .v-modal").show();      
+    });
+
+    $(".sendMessage").on('click', function () {
         var style = '<style>.displayFlex{display:flex;display:-webkit-flex;display:-moz-flex;display:-ms-flex;display:-o-flex}.notselect{user-select:none;-webkit-user-select:none;-moz-user-select:none;-ms-user-select:none;-o-user-select:none}.recordWrapper{max-width:1200px;height:auto;margin:0 auto;background-color:#fff}.my-record{padding:45px 10px 45px 8%}.my-record .user-info{display:flex;display:-webkit-flex;display:-moz-flex;display:-ms-flex;display:-o-flex;align-items:center;-webkit-align-items:center;-moz-align-items:center;-ms-align-items:center;-o-align-items:center}.my-record .portrait{border-radius:50%;-webkit-border-radius:50%;-moz-border-radius:50%;-ms-border-radius:50%;-o-border-radius:50%;width:60px;height:60px;object-fit:cover;-o-object-fit:cover}.my-record .portrait+strong{margin-left:15px;color:#333;font-size:20px}.my-record .info{margin:30px 0 90px;color:#333;font-size:16px}.my-record .info em{color:#49A5F8}.my-record .el-button{padding:17px 19px;font-size:16px;margin-left:-45px}.record-list .tab-nav{padding:40px 10px 0 15%;border-bottom:1px solid#E5E5E5}.record-list .tab-nav>a{display:inline-block;padding-bottom:15px;margin:0 5%;font-size:22px;color:#333}.record-list .tab-nav>a.current{color:#49A5F8;border-bottom:2px solid #49A5F8}.record-list .tab-nav>a:hover{color:#49A5F8}.record-list .tab-content{padding:30px 0}.record-list .tab-content .have-item{border:1px solid #E5E5E5;margin-bottom:20px}.record-list .tab-content .have-item .time{padding:8px 15px;color:#181818;font-size:18px;border-bottom:1px solid #E5E5E5}.record-list .tab-content .have-item .layout-box{display:flex;display:-webkit-flex;display:-moz-flex;display:-ms-flex;display:-o-flex;align-items:center;-webkit-align-items:center;-moz-align-items:center;-ms-align-items:center;-o-align-items:center;padding:15px 20px 15px 10px}.record-list .tab-content .have-item .layout-box .cover{width:250px;height:146px;margin-right:20px;background-repeat:no-repeat;background-position:center center;background-size:cover}.record-list .tab-content .have-item .layout-box .content{flex:1}.record-list .tab-content .have-item .layout-box .content .title{font-size:18px;color:#181818}.record-list .tab-content .have-item .layout-box .content .title:hover{color:#49A5F8}.record-list .tab-content .have-item .layout-box .content .goals{margin:12px 0;font-size:14px}.record-list .tab-content .have-item .layout-box .content .goals>span{color:#999}.record-list .tab-content .have-item .layout-box .content .goals>ul{padding-left:18px}.record-list .tab-content .have-item .layout-box .content .goals>ul li{margin-top:8px;color:#333}.record-list .tab-content .have-item .layout-box .content .foot{width:80%;display:flex;display:-webkit-flex;display:-moz-flex;display:-ms-flex;display:-o-flex;align-items:center;-webkit-align-items:center;-moz-align-items:center;-ms-align-items:center;-o-align-items:center;justify-content:space-between;-webkit-justify-content:space-between;-moz-justify-content:space-between;-ms-justify-content:space-between;-o-justify-content:space-between;color:#111}.record-list .tab-content .have-item .layout-box .content .foot span{margin-left:15px}.record-list .tab-content .have-item .layout-box .content .foot span em{color:#FF414A}.sort-btn{display:flex;display:-webkit-flex;display:-moz-flex;display:-ms-flex;display:-o-flex;padding:25px;font-size:16px;color:#333;cursor:pointer;user-select:none}.sort-btn .sort-icon{padding-right:5px}.sort-btn .sort-icon>i{display:block;line-height:8px;color:#CCC}.sort-btn .sort-icon>i.active{color:#49A5F8}.learned-record,.taughted-record{padding:45px 8%}.learned-record .item,.taughted-record .item{margin-bottom:40px;color:#111;font-size:16px;margin-right:3%}.learned-record .item>div,.taughted-record .item>div{margin-top:12px}.learned-record .item>span,.taughted-record .item>span{color:#111;font-weight:700}.learned-record .item em,.taughted-record .item em{color:#FF414A}.learnedDetails{width:100%;padding:80px 70px 60px;box-shadow:5px 4px 20px rgba(64,158,254,.44);position:relative}.learnedDetails::after,.learnedDetails::before{content:"";position:absolute;top:18px;z-index:2;width:20px;height:20px;background:#eb5a49;box-shadow:8px 4px 2px rgba(235,63,43,.44);border-radius:50%;-webkit-border-radius:50%;-moz-border-radius:50%;-ms-border-radius:50%;-o-border-radius:50%}.learnedDetails::after{left:25px}.learnedDetails::before{right:25px}.learnedDetails ul{list-style:none}.learnedDetails ul li{font-size:16px;color:#111;margin:20px 0}.learnedDetails em{color:#FF414A}.table-wrap{width:100%;margin-top:40px;border:1px solid #BFBFBF;border-radius:8px;-webkit-border-radius:8px;-moz-border-radius:8px;-ms-border-radius:8px;-o-border-radius:8px}.table-wrap td{height:50px;padding:8px 10px;text-align:center;font-size:14px}.table-wrap thead td{border-right:1px solid #BFBFBF;border-bottom:2px solid #bfbfbf;font-size:16px}.table-wrap thead td:last-child{border-right:none}.table-wrap .sort-btn{padding:0;justify-content:center;align-items:center}.table-wrap .sort-btn .sort-icon{padding-left:5px}.table-wrap tbody tr:nth-child(even){background:rgba(64,158,254,.1)}.table-wrap .user-img{width:40px;height:40px;margin:0 auto;border-radius:50%;-webkit-border-radius:50%;-moz-border-radius:50%;-ms-border-radius:50%;-o-border-radius:50%;background-size:contain;background-repeat:no-repeat;background-position:center center}.draw-user{height:30px;overflow:hidden}.draw-user::after,.draw-user::before{content:"";display:block;width:14px;height:14px;margin:0 auto;border:2px solid #49A5F8;border-radius:50%;-webkit-border-radius:50%;-moz-border-radius:50%;-ms-border-radius:50%;-o-border-radius:50%}.draw-user::after{width:24px;height:24px}.icon-more{transform:rotate(90deg);-webkit-transform:rotate(90deg);-moz-transform:rotate(90deg);-ms-transform:rotate(90deg);-o-transform:rotate(90deg);font-size:24px}.taughted-record{padding:40px 5%}.taughted-record .record-operate{text-align:right;margin-right:3%}.taughted-record .record-container{margin-top:20px;padding:30px;background:rgba(121,176,255,.05)}.taughted-record .item ol{padding-left:18px}.taughted-record .item ol li{margin-top:8px;color:#333}.taughted-record .item .chartLayout{display:flex;display:-webkit-flex;display:-moz-flex;display:-ms-flex;display:-o-flex}.taughted-record .item .chartLayout>div{width:50%}.taughted-record .student-info{display:flex;display:-webkit-flex;display:-moz-flex;display:-ms-flex;display:-o-flex}.taughted-record .student-taughted-details .express>span{margin-right:10px;font-size:14px;color:#676767}.taughted-record .student-taughted-details .express .r::before,.taughted-record .student-taughted-details .express .w::before{content:"";width:12px;height:12px;display:inline-block;margin:0 15px;vertical-align:middle;border-radius:50%;-webkit-border-radius:50%;-moz-border-radius:50%;-ms-border-radius:50%;-o-border-radius:50%}.taughted-record .student-taughted-details .express .r::before{background-color:#27CE2F}.taughted-record .student-taughted-details .express .w::before{background-color:#F53838}.taughted-record .student-taughted-details .table-wrap{margin-top:15px}.taughted-record .student-taughted-details .table-wrap .right{color:#27CE2F}.taughted-record .student-taughted-details .table-wrap .wrong{color:#F53838}@media (max-width:992px){.taughted-record{padding:20px}.taughted-record .record-container{padding:25px}}@media (max-width:768px){.record-list .tab-content{padding:20px 0}.record-list .tab-content .have-item .layout-box .content .foot{width:100%}.sort-btn{padding:15px}.learned-record .item,.taughted-record .item{margin-bottom:25px}.learned-record .item>div,.taughted-record .item>div{margin-top:8px}.table-wrap{margin-top:25px}.table-wrap .sort-btn,.table-wrap thead td{font-size:14px}.table-wrap td{padding:8px}}' +
         '.c3 svg{font:10px sans-serif;-webkit-tap-highlight-color:transparent}.c3 line,.c3 path{fill:none;stroke:#000}.c3 text{-webkit-user-select:none;-moz-user-select:none;user-select:none}.c3-bars path,.c3-event-rect,.c3-legend-item-tile,.c3-xgrid-focus,.c3-ygrid{shape-rendering:crispEdges}.c3-chart-arc path{stroke:#fff}.c3-chart-arc rect{stroke:#fff;stroke-width:1}.c3-chart-arc text{fill:#fff;font-size:13px}.c3-grid line{stroke:#aaa}.c3-grid text{fill:#aaa}.c3-xgrid,.c3-ygrid{stroke-dasharray:3 3}.c3-text.c3-empty{fill:grey;font-size:2em}.c3-line{stroke-width:1px}.c3-circle._expanded_{stroke-width:1px;stroke:#fff}.c3-selected-circle{fill:#fff;stroke-width:2px}.c3-bar{stroke-width:0}.c3-bar._expanded_{fill-opacity:1;fill-opacity:.75}.c3-target.c3-focused{opacity:1}.c3-target.c3-focused path.c3-line,.c3-target.c3-focused path.c3-step{stroke-width:2px}.c3-target.c3-defocused{opacity:.3!important}.c3-region{fill:#4682b4;fill-opacity:.1}.c3-brush .extent{fill-opacity:.1}.c3-legend-item{font-size:12px}.c3-legend-item-hidden{opacity:.15}.c3-legend-background{opacity:.75;fill:#fff;stroke:#d3d3d3;stroke-width:1}.c3-title{font:14px sans-serif}.c3-tooltip-container{z-index:10}.c3-tooltip{border-collapse:collapse;border-spacing:0;background-color:#fff;empty-cells:show;-webkit-box-shadow:7px 7px 12px -9px #777;-moz-box-shadow:7px 7px 12px -9px #777;box-shadow:7px 7px 12px -9px #777;opacity:.9}.c3-tooltip tr{border:1px solid #ccc}.c3-tooltip th{background-color:#aaa;font-size:14px;padding:2px 5px;text-align:left;color:#fff}.c3-tooltip td{font-size:13px;padding:3px 6px;background-color:#fff;border-left:1px dotted #999}.c3-tooltip td>span{display:inline-block;width:10px;height:10px;margin-right:6px}.c3-tooltip td.value{text-align:right}.c3-area{stroke-width:0;opacity:.2}.c3-chart-arcs-title{dominant-baseline:middle;font-size:1.3em}.c3-chart-arcs .c3-chart-arcs-background{fill:#e0e0e0;stroke:#fff}.c3-chart-arcs .c3-chart-arcs-gauge-unit{fill:#000;font-size:16px}.c3-chart-arcs .c3-chart-arcs-gauge-max{fill:#777}.c3-chart-arcs .c3-chart-arcs-gauge-min{fill:#777}.c3-chart-arc .c3-gauge-value{fill:#000}.c3-chart-arc.c3-target g path{opacity:1}.c3-chart-arc.c3-target.c3-focused g path{opacity:1}'+
-        '.noprint{display: none;}svg{height:600px;width:100%}</style>'
-        if(email) {  
-            $.post('/api/record/sendEmail', {
-                email: email,
-                content: '<div class="recordWrapper taughted-record">' + $('.record-container').html() + '</div>' + style
-            }, function(response) {
-                console.log(response);
-            })
-        } 
+        '.noprint{display: none;}svg{height:600px;width:100%}</style>';
+
+        $.post('/api/record/sendEmail', {
+            email: $("#emailInput").val(),
+            content: '<div class="recordWrapper taughted-record">' + $('.record-container').html() + '</div>' + style
+        }, function(response) {
+            $(".el-message-box__wrapper , .v-modal").fadeOut(500);
+            console.log(response);
+        })
     });
+
+    $(".closeMessage").on('click', function() {
+        $(".el-message-box__wrapper , .v-modal").fadeOut(500);    
+    })
+
     $('#btnPrint').on('click', function() {
         if(lessonUrl) {
             $('.recordWrapper').append('<iframe id="keepworkContainer" frameborder="0" width="100%" src="' + keepworkHost + lessonUrl + '?device=print"></iframe>');
@@ -202,12 +211,17 @@ var getLessonTaughtedRecord = function() {
         classId: classId
     }, function (response) {
         var r = response.data;
+        console.log(response);
+
         if (response.err == 0) {
             lessonUrl = r.lessonUrl;
             $('.lesson-no').text(r.lessonNo);
             $('.lesson-title').text(r.lessonTitle);
             $('.lesson-time').text( fmtDate(r.startTime) );
             $('.lesson-goals').text(r.goals);
+            $(".lecture-date").html("<strong> "+  fmtDate(r.startTime) + "</strong>");
+            $(".teacher-nickname").text("Teacher: " + r.teacher + ".");
+            $(".student-num").text(r.summary.length);
             if(r.summary instanceof Array) {
                 summary = r.summary;
                 var quizzLable = []; // quizz 图表的 Lable
@@ -294,9 +308,11 @@ function calcPageHeight(doc) {
 window.onload = function() {
     var height = calcPageHeight(document);
     var container = parent.document.getElementById('summaryContainer');
-    container.style.height = height + 'px';
-    container.contentWindow.document.getElementsByClassName("header")[0].style.display = 'none'; 
-    container.contentWindow.document.getElementsByClassName("main")[0].style.padding = "0";
-    container.contentWindow.document.getElementsByClassName("recordWrapper")[0].style.width = '1080px';
-    container.contentWindow.document.getElementsByClassName("recordWrapper")[0].style.padding = "40px 2%";
+    if(container) {
+        container.style.height = height + 'px';
+        container.contentWindow.document.getElementsByClassName("header")[0].style.display = 'none'; 
+        container.contentWindow.document.getElementsByClassName("main")[0].style.padding = "0";
+        container.contentWindow.document.getElementsByClassName("recordWrapper")[0].style.width = '1080px';
+        container.contentWindow.document.getElementsByClassName("recordWrapper")[0].style.padding = "40px 2%";
+    }
 }
