02cec975c73674b1565b1929de7a592a3446dbcd
xiaop1ng <784516419@qq.com>
2018-06-01 13:48:36 +0800

    添加了课程包相关的接口

--- api/subscribe.lua
+++ api/subscribe.lua
@@ -50,14 +50,42 @@ end)
 -- 获取课程包的学习与购买状态
 router:get('/state', function(req, res, next)
     local p = req.query
-    local rs = {}
     local packageId = p.packageId
     local rq = rq(p, {'packageId'}, res)
     if(not rq) then return end
     local token = req.cookies.token
 
     local getMineState = function(user)
-
+        -- 先检测购买状态，再检测学习状态
+        local object = {
+            username = user.username,
+            packageId = packageId
+        }
+        local subscribeVo = subscribeBll.get(object)
+        if(subscribeVo) then
+            -- 已添加课程包
+            local packageState = subscribeBll.state({
+                ['sb.username'] = user.username,
+                ['sb.packageId'] = packageId
+            })
+            if(packageState) then
+                packageState.lessons = commonlib.Json.Decode(packageState.lessons)
+                res:send({
+                    err = 0,
+                    data = packageState
+                })
+            else
+                res:send({
+                    err = 101,
+                    msg = 'get mine package state fail.'
+                })
+            end
+        else
+            res:send({
+                err = 400,
+                msg = 'package state: no bought.'
+            })
+        end
     end
 
     commonBll.auth(token, getMineState, function()
