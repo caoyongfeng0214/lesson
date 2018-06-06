5edde8ce260ba9d0d92521e3ac60ade365dcfd8e
yuan <mooonyuan@gmail.com>
2018-06-06 18:38:12 +0800

    学习记录列表

--- public_dev/js/common.js
+++ public_dev/js/common.js
@@ -76,32 +76,78 @@ var fmtDate = function( dateStr , fmtStr ) {
 
 /* 
     open dialog
-
+    @params  label string
+             message string 
+             input boolean
+             cancelBtn boolean
+             callback return input.val
+             messageClass
+             height
+             width
 */
-// var openDialog = function( params ){
-//     var str = +'<div tabindex="-1" role="dialog" aria-modal="true" aria-label="提示" class="el-message-box__wrapper" style="z-index: 2081;">'
-//                     +'<div class="el-message-box el-message-box--center">'
-//                     + '<div class="el-message-box__header">'
-//                         +    '<button type="button" aria-label="Close" class="el-message-box__headerbtn">'
-//                         +       '<i class="el-message-box__close el-icon-close"></i>'
-//                         +  '</button>'
-//                     +' </div>'
-//                         +'<div class="el-message-box__content">'
-//                         + '  <div class="el-message-box__message">'
-//                         +      '<p>此操作将永久删除该文件, 是否继续?</p>'
-//                         + '</div>'
-//                     +' </div>'
-//                         +'<div class="el-message-box__btns">'
-//                         + '  <button type="button" class="el-button el-button--default el-button--small">'
-//                         +     ' <span>取消</span>'
-//                         +' </button>'
-//                         +'  <button type="button" class="el-button el-button--default el-button--small el-button--primary ">'
-//                         +     '<span>确定</span>'
-//                         +'  </button>'
-//                         +'</div>'
-//                 +' </div>'
-//                 +'</div>'
-//                 +'<div class="v-modal" tabindex="0" style="z-index: 2080;"></div>'
-//             $('.main').append(str);
+var openDialog = function( params,callback ){
+    // 弹窗标题
+    params.label = params.label ? params.label :'';
+    //弹窗信息
+    params.message = params.message ? params.message :'';
+    // 添加弹窗信息class
+    params.messageClass = params.messageClass ? params.messageClass : ''
+ 
+    //弹窗内容
+    var str = '<div tabindex="-1" role="dialog" aria-modal="true" aria-label="'+ params.label +'" class="open-dialog el-message-box__wrapper" style="z-index: 2081;">'
+            +'<div class="el-message-box el-message-box--center" style="height:'+ params.height +';width:'+ params.width +'">'
+            +'<div class="el-message-box__header">'
+            +'<button type="button" aria-label="Close" class="close-icon el-message-box__headerbtn">'
+            +'<i class="el-message-box__close el-icon-close"></i>'
+            + '</button>'
+            +'</div>'
+            +'<div class="el-message-box__content">'
+            + '<div class="el-message-box__message '+ params.messageClass +'">'
+            +  '<p>'+ params.message +'</p>'
+            + '</div>';
+    //需要输入框
+    if( params.input ){
+        str += '<div class="el-message-box__input">'
+        + '<div class="el-input">'
+        + '<input type="text" name="openInput" autocomplete="off" placeholder="" class="el-input__inner">'
+        + '</div>'
+        + '<div class="el-message-box__errormsg" style="visibility: hidden;"></div>'
+        + '</div>';
+    }
 
-// }
+    str += '</div>'
+        +'<div class="el-message-box__btns">'
+
+    //需要取消按钮
+    if( params.cancelBtn ){
+        str += '<button type="button" class="cancel-btn el-button el-button--default el-button--small">'
+        + '<span>取消</span>'
+        +'</button>';;
+    }
+            
+    str += '<button type="button" class="ok-btn el-button el-button--default el-button--small el-button--primary ">'
+    +   '<span>确定</span>'
+    +'</button>'
+    +'</div>'
+    +' </div>'
+    +'</div>'
+    +'<div class="v-modal" tabindex="0" style="z-index: 2080;"></div>'
+
+    $('.main').append(str);
+
+    //相关操作
+    //关闭弹窗
+    
+    $( '.open-dialog .close-icon,.open-dialog .cancel-btn' ).on('click',function(){
+        $('.open-dialog,.v-modal').fadeOut(500);
+    })
+
+    //确定
+    $( '.open-dialog .ok-btn' ).on('click',function(){
+        if( $('.open-dialog input[name="openInput"]').val() ){
+            callback($('.open-dialog input[name="openInput"]').val());
+        }else{
+            $('.open-dialog,.v-modal').fadeOut(500);
+        }
+    })
+}
